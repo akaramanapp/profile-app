e6a07ee9803818c254df45782f7f7b40b8cb0781
abdulkerim.karaman <abdulkerim.karaman@albarakatech.com>
2023-08-13 20:39:12 +0300

    change strings

--- src/Shared/Strings.tsx
+++ src/Shared/Strings.tsx
@@ -1,4 +1,14 @@
-export default {
+export default{
     ABOUT_HEADER: "Full Stack Developer",
     ABOUT_DESC: 'I am a software engineer with a passion for building things. I am currently working at Albaraka Global Tech as a full stack developer. Gazi Universty Institute of Technology graduate a BS in Information Technology. I am interested in full-stack development, mobile development, and back end services. I am also interested in the intersection of technology and finance.',
+    
+    SERVICES:'Services',
+    FRONTED:'FRONTED DEV',
+    FRONTED_DESC:'Elevate your online presence with captivating user interfaces and seamless experiences',
+
+    BACKEND:'BACKEND DEV',
+    BACKEND_DESC:'Developing application backend and Server side development using various backend technology',
+
+    ONLINE:'ONLINE TEACHING',
+    ONLINE_DESC:'Teaching programming and coding on various platform like Youtube, Udemy etc. Free Course to the student'
 }
