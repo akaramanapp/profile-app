8c06627e36a8d90c692dec99c29bdfed9ae438e1
abdulkerim.karaman <abdulkerim.karaman@albarakatech.com>
2023-08-13 21:24:59 +0300

    update profile images

--- src/Components/ProfilePic.tsx
+++ src/Components/ProfilePic.tsx
@@ -1,6 +1,6 @@
 function ProfilePic() {
     return <div className="fixed mt-[90px]">
-        <img src="/https://raw.githubusercontent.com/akaramanapp/profile-app/main/dist/user.png" className="h-screen object-cover w-screen" />
+        <img src="https://raw.githubusercontent.com/akaramanapp/profile-app/main/dist/user.png" className="h-screen object-cover w-screen" />
     </div>;
 }
 
